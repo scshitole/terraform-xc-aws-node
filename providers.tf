8cc414519fc8b3b16d5123d0e268d4b8586a1fdf
Sanjay Shitole <scshitole@gmail.com>
2023-05-18 15:42:51 -0700

    update

--- providers.tf
+++ providers.tf
@@ -13,16 +13,3 @@ terraform {
     }
 }
 
-/*provider "volterra" {
-  api_p12_file = var.api_p12_file
-  api_cert     = var.api_p12_file != "" ? "" : var.api_cert
-  api_key      = var.api_p12_file != "" ? "" : var.api_key
-  api_ca_cert  = var.api_ca_cert
-  url          = var.api_url
-}
-
-provider "aws" {
-  region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-}*/
